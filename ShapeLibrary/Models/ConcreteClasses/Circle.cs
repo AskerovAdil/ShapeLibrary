e992c3e3ffca730bf7445617405b5391e9777943
AskerovAdil <Askerov121099@mail.ru>
2023-06-07 18:16:09 +0700

    feat--shape-model

---
+++ ShapeLibrary/Models/ConcreteClasses/Circle.cs
@@ -0,0 +1,20 @@
+﻿using ShapeLibrary.Models.AbstractClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary.Models.ConcreteClasses
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+}
---
+++ ShapeLibrary/Models/ConcreteClasses/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using ShapeLibrary.Models.AbstractClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary.Models.ConcreteClasses
+{
+    public class Triangle : Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public override double CalculateArea()
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public bool IsRightAngled()
+        {
+            double[] sides = { SideA, SideB, SideC };
+            Array.Sort(sides);
+            return Math.Pow(sides[2], 2) == Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2);
+        }
+    }
+}
